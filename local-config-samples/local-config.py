0c7bdde41b93d288fb2d3d3130d60cec6c7b746b
Param Siddharth <contact@paramsid.com>
2021-06-10 20:39:04 +0530

    Add samples for local configurations

---
+++ local-config-samples/local-config.py
@@ -0,0 +1,6 @@
+STORAGE_DIR = 'D:\\MyProjects\\_Non-Project\\Internship\\ProtonAutoML\\storage'
+BACKEND_URI = 'http://localhost:5000/'
+MONGO_URI = 'mongodb://localhost:27017/devProton'
+# STORAGE_DIR = '/home/ubuntu/storage'
+# BACKEND_URI = 'https://app.protonautoml.com/api/' # The trailing slash is important
+# MONGO_URI = 'mongodb+srv://url/of/devProton'
